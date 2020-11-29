4787587ffe95b96eb1abc3fa2515b248b5a6a586
Edgar Zhivaev <edyapups@gmail.com>
2020-11-29 13:08:02 +0300

    healthz endpoint implemented.

---
+++ app.py
@@ -0,0 +1,48 @@
+import fastapi
+from utils import is_alive_host
+from aiohttp import ClientSession
+from typing import Optional
+
+
+class FastAPI(fastapi.FastAPI):
+    client: ClientSession
+    healthz_returns = {
+        True: 'up',
+        False: 'down',
+    }
+
+
+app = FastAPI()
+
+
+@app.on_event('startup')
+async def open_client_session():
+    """
+    Initializes a session for outgoing requests when the application starts.
+    """
+    app.client = ClientSession()
+
+
+@app.get('/healthz')
+async def healthz(host: str):
+    """
+    Returns:
+        {
+            'status': on | off
+        }
+
+    up: if the received host returns http status between 100 and 400.
+    down: otherwise.
+    """
+    result = await is_alive_host(host, app.client)
+    return {
+        'status': app.healthz_returns[result],
+    }
+
+
+@app.on_event('shutdown')
+async def close_client_session():
+    """
+    Closes the session for outgoing requests when the application stops.
+    """
+    await app.client.close()
