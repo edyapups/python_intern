3ec96b81b9cff688daebc8a2d22c454b21157ba5
Edgar Zhivaev <edyapups@gmail.com>
2020-11-29 18:39:21 +0300

    Implemented errors check.

--- app/utils.py
+++ app/utils.py
@@ -1,7 +1,11 @@
 from aiohttp import ClientSession
+from aiohttp.client_exceptions import ClientError
 
 
 async def is_alive_host(hostname: str, client: ClientSession) -> bool:
     """Проверить, что запрашиваемый хост возвращает http status 100<=x<400."""
-    result = await client.get(hostname)
-    return 100 <= result.status <= 400
+    try:
+        result = await client.get(hostname)
+        return 100 <= result.status <= 400
+    except ClientError:
+        return False
