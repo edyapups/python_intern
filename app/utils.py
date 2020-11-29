a18a25e55d9c694e723d54423576ff25ecd71513
Edgar Zhivaev <edyapups@gmail.com>
2020-11-29 20:04:25 +0300

    Fixed url.

--- app/utils.py
+++ app/utils.py
@@ -5,7 +5,7 @@ from aiohttp.client_exceptions import ClientError
 async def is_alive_host(hostname: str, client: ClientSession) -> bool:
     """Проверить, что запрашиваемый хост возвращает http status 100<=x<400."""
     try:
-        result = await client.get(hostname)
+        result = await client.get('http://' + hostname)
         return 100 <= result.status <= 400
     except ClientError:
         return False
